ba56235051b6b90a0d2d590d3687817c7b590233
Adrian <curia.gheorghe@yahoo.com>
2020-12-12 22:00:11 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CategoryPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
